e8d8d0d42f06fdf7e1d1817fee4ad94c74b0a469
swathimeenal <swathimeenal1994@gmail.com>
2024-01-10 02:45:31 +0530

    task completed

---
+++ script.js
@@ -0,0 +1,62 @@
+setTimeout(()=>{
+    console.log("10")
+    document.getElementById("display").innerText="10"
+    document.body.style.backgroundColor = "red";
+    setTimeout(()=>{
+        console.log("9")
+        document.getElementById("display").innerText="9"
+        document.body.style.backgroundColor = "blue";
+        setTimeout(()=>{
+            console.log("8")
+            document.getElementById("display").innerText="8"
+            document.body.style.backgroundColor = "orange";
+            setTimeout(()=>{
+                console.log("7")
+                document.getElementById("display").innerText="7"
+                document.body.style.backgroundColor = "blueviolet";
+                setTimeout(()=>{
+                    console.log("6")
+                    document.getElementById("display").innerText="6"
+                    document.body.style.backgroundColor = "green";
+                    setTimeout(()=>{
+                        console.log("5")
+                        document.getElementById("display").innerText="5"
+                        document.body.style.backgroundColor = "yellow";
+                        setTimeout(()=>{
+                            console.log("4")
+                            document.getElementById("display").innerText="4"
+                            document.body.style.backgroundColor = "pink";
+                            setTimeout(()=>{
+                                console.log("3")
+                                document.getElementById("display").innerText="3"
+                                document.body.style.backgroundColor = "white";
+                                setTimeout(()=>{
+                                    console.log("2")
+                                    document.getElementById("display").innerText="2"
+                                    document.body.style.backgroundColor = "red";
+                                    setTimeout(()=>{
+                                        console.log("1")
+                                        document.getElementById("display").innerText="1"
+                                        document.body.style.backgroundColor = "yellow";
+                                        setTimeout(()=>{
+                                            console.log("1")
+                                            document.getElementById("display").innerText="Happy Independence day💐😎"
+                                            document.body.style.backgroundColor = "orange";
+                                        },1000)
+    },1000)
+    
+    },1000)
+    
+    },1000)
+    
+    },1000)
+    },1000)
+    
+    },1000)
+    
+    },1000)
+    
+    },1000)
+    
+    },1000)
+},1000)
